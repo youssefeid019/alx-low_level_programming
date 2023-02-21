e18b6de61a00508630d2c9220473ce0a4731d570
Youssef Eid <youssef20191700778@cis.asu.edu.eg>
2023-02-22 00:26:21 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the string "_putchar" followed by a newline character
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 printf("_putchar\n");
-return 0;
+return (0);
 }
