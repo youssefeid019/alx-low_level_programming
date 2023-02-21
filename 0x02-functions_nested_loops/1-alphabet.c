867c3a9fc6091089ae2d4cc09b4d60a7680e8730
Youssef Eid <youssef20191700778@cis.asu.edu.eg>
2023-02-22 00:37:23 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line
  *
  * Description: Prints the English alphabet in lowercase, followed by a new line.
- * Uses the _putchar function to print each character.
+ *              Uses the _putchar function to print each character.
  */
 void print_alphabet(void)
 {
 char letter = 'a';
+
 while (letter <= 'z')
 {
 _putchar(letter);
 letter++;
 }
+
 _putchar('\n');
 }
