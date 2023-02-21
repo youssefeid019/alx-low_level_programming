8559024c7a5855412efa93a6c746416475baab5e
Youssef Eid <youssef20191700778@cis.asu.edu.eg>
2023-02-22 01:33:47 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,17 +11,22 @@ void print_to_98(int n)
 {
 int i;
 
-if (n <= 98) {
-for (i = n; i <= 98; i++) {
+if (n <= 98) 
+{
+for (i = n; i <= 98; i++) 
+{
 printf("%d", i);
-if (i != 98) {
+if (i != 98) 
+{
 printf(", ");
 }
 }
 } else {
-for (i = n; i >= 98; i--) {
+for (i = n; i >= 98; i--) 
+{
 printf("%d", i);
-if (i != 98) {
+if (i != 98) 
+{
 printf(", ");
 }
 }
