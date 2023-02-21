afd90b50de91b5a34576d0ad4a666e4ebec7c832
Youssef Eid <youssef20191700778@cis.asu.edu.eg>
2023-02-22 00:33:18 +0200

    Add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Description: Prints the English alphabet in lowercase, followed by a new line.
+ * Uses the _putchar function to print each character.
+ */
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
