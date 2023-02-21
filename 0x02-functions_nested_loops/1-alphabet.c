7820d8691a96fffae2b01a45868770e04990328c
Youssef Eid <youssef20191700778@cis.asu.edu.eg>
2023-02-22 00:42:34 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,8 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase, followed by a new line
  *
- * Description: Prints the English alphabet in lowercase, 
- * followed by a new line.
- * Uses the _putchar function to print each character.
+ * Description: Prints the English alphabet in lowercase, followed by a new line.
+ *              Uses the _putchar function to print each character.
  */
 void print_alphabet(void)
 {
