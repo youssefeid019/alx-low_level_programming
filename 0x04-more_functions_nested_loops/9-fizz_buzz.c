8df9f0f20aaf882ec3be8ebbd2d6f6824e9bbd2d
Youssef Eid <youssef20191700778@cis.asu.edu.eg>
2023-02-24 03:01:54 +0200

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints the numbers from 1 to 100, replacing multiples of 3
- * with "Fizz", multiples of 5 with "Buzz", and multiples of both with "FizzBuzz"
+ * main - prints numbers from 1 to 100, replacing multiples of 3 with "Fizz",
+ *        multiples of 5 with "Buzz", and multiples of both with "FizzBuzz"
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-for (int i = 1; i <= 100; i++)
+int i;
+
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-{
 printf("FizzBuzz ");
-}
 else if (i % 3 == 0)
-{
 printf("Fizz ");
-}
 else if (i % 5 == 0)
-{
 printf("Buzz ");
-}
 else
-{
 printf("%d ", i);
 }
-}
+
 printf("\n");
+
 return (0);
 }
