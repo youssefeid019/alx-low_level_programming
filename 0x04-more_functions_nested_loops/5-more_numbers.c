96289ac85f135dc7e2ddc7c23ba6a4de5faf2d20
Youssef Eid <youssef20191700778@cis.asu.edu.eg>
2023-02-24 02:40:43 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers 0-9 except 2 and 4
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  */
-
 void more_numbers(void)
 {
-int num;
+int i, j;
 
-for (num = 0; num <= 9; num++)
+for (i = 0; i < 10; i++)
 {
-if (num != 2 && num != 4)
-_putchar(num + '0');
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 }
 _putchar('\n');
+}
 }
