e5fe0fd30489fd036660e6f972313a1327de8696
Youssef Eid <youssef20191700778@cis.asu.edu.eg>
2023-02-24 02:58:45 +0200

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,23 @@
 
 int main(void)
 {
-int i;
-
-for (i = 1; i <= 100; i++)
-{
-if ((i % 3 == 0) && (i % 5 == 0))
+for (int i = 1; i <= 100; i++) {
+if (i % 3 == 0 && i % 5 == 0) {
 printf("FizzBuzz ");
+}
 else if (i % 3 == 0)
+{
 printf("Fizz ");
+}
 else if (i % 5 == 0)
+{
 printf("Buzz ");
+}
 else
+{
 printf("%d ", i);
 }
-
+}
 printf("\n");
-
 return (0);
 }
