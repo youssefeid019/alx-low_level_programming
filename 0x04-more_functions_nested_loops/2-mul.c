b7a0aabcc89b11ec759c5be2ce423d0fe6eb97d6
Youssef Eid <youssef20191700778@cis.asu.edu.eg>
2023-02-24 02:24:38 +0200

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first integer to multiply
+ * @b: second integer to multiply
+ *
+ * Return: the result of multiplying a and b
+ */
+int mul(int a, int b)
+{
+return a * b;
+}
