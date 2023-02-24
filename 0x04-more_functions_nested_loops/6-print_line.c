95f84194722b7f50b080d9a1e81e648134c3398a
Youssef Eid <youssef20191700778@cis.asu.edu.eg>
2023-02-24 02:42:54 +0200

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+return();
+}
+while (n--)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
